eb8db35e1e5ffcc9bcf35e88b520243323053ed0
Am-Delta <140981321+Am-Delta@users.noreply.github.com>
2023-09-02 05:54:33 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ echo "Running ..."
 sudo apt install python3
 sudo apt install pip
 sudo apt install git
-pip install -r r.txt
 
 mkdir /root/botCh
 
@@ -15,9 +14,9 @@ mv /root/bot/ssh /root/botCh
 mv /root/bot/cache /root/botCh
 mv /root/bot/logs.txt /root/botCh
 mv /root/bot/All.txt /root/botCh
-mv /root/bot/irr.txt /root/botCh
 mv /root/bot/ssh.db /root/botCh
 mv /root/bot/data.json /root/botch
+mv /root/bot/irr.txt /root/botCh
 
 rm -r /root/bot
 
@@ -30,12 +29,14 @@ mv /root/botCh/ssh /root/bot
 mv /root/botCh/cache /root/bot
 mv /root/botCh/logs.txt /root/bot
 mv /root/botCh/All.txt /root/bot
-mv /root/botCh/irr.txt /root/bot
 mv /root/botCh/ssh.db /root/bot
 mv /root/botCh/data.json /root/bot
+mv /root/botCh/irr.txt /root/bot
 
 
 cd /root/bot
+
+pip install -r r.txt
 
 #clear
 
