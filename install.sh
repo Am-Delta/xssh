4f6ee511dd5c61e0202fa65aad14179df4eb2e47
Am-Delta <140981321+Am-Delta@users.noreply.github.com>
2023-10-02 16:29:15 +0330

    Bugs fixed

---
+++ install.sh
@@ -0,0 +1,60 @@
+echo "Running ..."
+
+sudo apt install python3
+sudo apt install pip
+sudo apt install git
+pip install pyrogram
+pip install random2
+pip install qrcode
+pip install cryptocompare
+pip install tgcrypto
+pip install selectolax
+pip install bs4
+pip install requests
+pip install termcolor
+pip install Unidecode
+sudo apt install python3-random2
+sudo apt install python3-qrcode
+sudo apt install python3-bs4
+sudo apt install python3-requests
+sudo apt install python3-termcolor
+
+mkdir /root/botCh
+
+pkill -9 python3
+pkill -9 python
+
+mv /root/bot/Pannels.txt /root/botCh
+mv /root/bot/backup /root/botCh
+mv /root/bot/ssh /root/botCh
+mv /root/bot/cache /root/botCh
+mv /root/bot/logs.txt /root/botCh
+mv /root/bot/All.txt /root/botCh
+mv /root/bot/ssh.db /root/botCh
+mv /root/bot/data.json /root/
+
+rm -r /root/bot
+
+git clone "https://github.com/Am-Delta/xssh.git" /root/bot/
+
+mv /root/botCh/Pannels.txt /root/bot
+mv /root/botCh/backup /root/bot
+mv /root/botCh/ssh /root/bot
+mv /root/botCh/cache /root/bot
+mv /root/botCh/logs.txt /root/bot
+mv /root/botCh/All.txt /root/bot
+mv /root/botCh/ssh.db /root/bot
+mv /root/data.json /root/bot/
+
+rm -r /root/botCh
+
+cd /root/bot
+
+clear
+
+chmod 664 /root/bot/
+chmod 664 /usr/local/lib/
+
+python3 run.py
+
+echo -ne '\n\n'
---
+++ run.py
@@ -0,0 +1,143 @@
+import os
+import re
+import json
+import sqlite3
+from termcolor import colored
+from pathlib import Path
+
+
+if not os.path.exists('ssh'):
+    os.makedirs('ssh')
+
+if not os.path.exists('backup'):
+    os.makedirs('backup')
+
+if not os.path.exists('cache'):
+    os.makedirs('cache')
+
+
+new_dict = {}
+file = "data.json"
+
+
+def db_update():
+    conn = sqlite3.connect('ssh.db', check_same_thread=False)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Settings WHERE ID=:ID", {'ID': 1})
+    records = cur.fetchall()
+    s = records[0][1]
+    s = s.replace("\'", "\"")
+    p = re.compile('(?<!\\\\)\'')
+    s = p.sub('\"', s)
+    settings = json.loads(s)
+    if settings.get("test", None) is None:
+        add_dict = {
+            "test": "off",
+            "test-traffic": 256,
+            "buy-traffic": "off"
+        }
+        settings.update(add_dict)
+        cur.execute("UPDATE Settings SET settings = ? WHERE ID =?", (str(settings), 1))
+        conn.commit()
+    try:
+        cur.execute("SELECT * FROM Tests")
+        records = cur.fetchall()
+    except sqlite3.OperationalError:
+        cur.execute("""CREATE TABLE Tests (
+            ID int,
+            'date' text
+            )""")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def run():
+    if Path("Pannels.txt").is_file() is False:
+        open("Pannels.txt", "w")
+    if Path("All.txt").is_file() is False:
+        with open("All.txt", "a+") as f:
+            f.writelines("1" + "\n")
+    if Path("ssh.db").is_file() is False:
+        os.system('python3 sshdb.py')
+        print("Database Created, change the settings in the bot")
+    db_update()
+    print(colored("\nRunning the bot... if you see any issues run the command again.\n\nif you want stop to the bot use this command:\n\npkill -9 python3 or pkill -9 python\n\nYou can now close the window.", 'white'))
+    os.system('nohup python3 session-updater.py &')
+    os.system('nohup python3 backup-ssh.py &')
+    os.system('nohup python3 bot.py &')
+    os.remove('sshdb.py')
+
+
+def settings():
+    print(colored("\nType admin id, if you want add multiple admins type like this: 123456 654321 789456", 'white'))
+    while True:
+        data = input("ids: ")
+        if " " not in data:
+            try:
+                admin_id = [int(data)]
+                break
+            except:
+                print(colored("[-] Error: Type the correct value only numbers", 'red'))
+        else:
+            admin_id = []
+            for admin in data.split(" "):
+                try:
+                    admin_id.append(int(admin))
+                    status = True
+                except:
+                    status = False
+                    print(colored("[-] Error: Type the correct value only numbers", 'red'))
+                    break
+            if status is True:
+                break
+
+    print(colored("\nif you want send your api_id or (default: n)", 'white'))
+    while True:
+        data = input(": ")
+        if data == "n" or data == "":
+            api_id = "24467048"
+            api_hash = "7e81e7df9b9b66deab287cb47bbc6d8a"
+            api = True
+            break
+        else:
+            try:
+                int(data)
+                api_id = data
+                api = False
+                break
+            except:
+                print(colored("[-] Error: Type the correct value only numbers", 'red'))
+    if api is False:
+        print(colored("\napi_hash: ", 'white'))
+        api_hash = input("")
+    print(colored("\nBot Token: ", 'white'))
+    Token = input("")
+
+    add_dict = {
+        "admin": admin_id,
+        "api_id": api_id,
+        "api_hash": api_hash,
+        "Token": Token
+    }
+    new_dict.update(add_dict)
+
+    if Path(file).is_file() is True:
+        os.remove(file)
+        print(colored("\nData renewed", 'cyan'))
+    with open(file, 'w') as outfile:
+        json.dump(new_dict, outfile)
+        print(colored("\nSaved data in " + file, 'cyan'))
+
+
+if Path(file).is_file() is True:
+    print(colored("\nThe old settings exist, Do you want to change the settings? send y to change (Default: n)", 'cyan'))
+    job = input("")
+    if job == 'y':
+        settings()
+        run()
+    else:
+        run()
+else:
+    settings()
+    run()
