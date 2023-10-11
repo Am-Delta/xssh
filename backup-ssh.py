71239e78b44e2ba01955e9bab7533808d960c785
Am-Delta <140981321+Am-Delta@users.noreply.github.com>
2023-10-11 13:48:54 -0700

    Update backup-ssh.py

--- backup-ssh.py
+++ backup-ssh.py
@@ -1,49 +1,32 @@
+import os
 import sshx
-from os import makedirs, remove
 from time import sleep
 from pathlib import Path
 
 folder = 'backup'
 sleeper = 3600 * 6
 
-try:
-    makedirs(folder)
-except:
-    pass
-
-#host@username:passwd   <------------------------------------ #
+if os.path.isdir(folder) is False:
+    os.makedirs(folder)
 
 
 def main():
-    with open("Pannels.txt", 'r') as txt:
-        for data in txt.readlines():
-            do = True
-            data = data.replace('\n', "")
-            host = data.split("@")[0]
-            username = (data.split(":")[0]).split("@")[1]
-            password = data.split(":")[1]
-            session = 'ssh/' + host + ".session"
-            if Path(session).is_file() is False:
-                if sshx.Login(username, password, host) is False:
-                    do = False
-            if do is True:
-                try:
-                    Session = sshx.PANNEL(host, username, password, 'Other', 'uname')
-                    status, content = Session.Backup_content()
-                    if status is True:
-                        f = folder + "/" + host + ".sql"
-                        if Path(f).is_file() is True:
-                            remove(f)
-                        with open(f, 'wb') as file:
-                            file.write(content)
-                        sleep(1)
-                    else:
-                        print("[-] Error: " + content + " | " + host)
-                except Exception as e:
-                    print("[-] Error: " + str(e) + " | " + host)
+    hosts = sshx.HOSTS()
+    for host in hosts:
+        port, username, password, panel, route_path, sshport, udgpw = sshx.HOST_INFO(host)
+        try:
+            Session = sshx.PANNEL(host, username, password, port, panel, 'Other', 'uname')
+            status, content = Session.Backup_content()
+            if status is True:
+                f = folder + "/" + host + ".sql"
+                if Path(f).is_file() is True:
+                    os.remove(f)
+                with open(f, 'wb') as file:
+                    file.write(content)
             else:
-                print("[-] Error To Login: " + host)
-
+                print("[-] Error: " + content + " | " + host)
+        except Exception as e:
+            print("[-] Error: " + str(e) + " | " + host)
 
 while True:
     main()
