df219ea608f0327751272bd452aab80d892d8009
Am-Delta <140981321+Am-Delta@users.noreply.github.com>
2023-10-15 21:51:15 -0700

    Update session-updater.py

--- session-updater.py
+++ session-updater.py
@@ -3,8 +3,7 @@ from pathlib import Path
 from os import remove
 from time import sleep
 from datetime import datetime
-
-sleeper = 600
+from random import randint
 
 if Path('logs.txt').is_file() is True:
     remove('logs.txt')
@@ -12,9 +11,9 @@ if Path('logs.txt').is_file() is True:
 
 def main():
     with open('logs.txt', 'a+') as logs:
-        hosts = sshx.HOSTS()
+        hosts, remarks = sshx.HOSTS()
         for host in hosts:
-            port, username, password, panel, route_path, sshport, udgpw = sshx.HOST_INFO(host)
+            port, username, password, panel, route_path, sshport, udgpw, remark = sshx.HOST_INFO(host)
             do = True
             session = 'ssh/' + host + ".session"
             if Path(session).is_file() is False:
@@ -24,14 +23,15 @@ def main():
                 try:
                     url, r = sshx.open_session(host, port)
                     if sshx.Test(r, host, port, panel, 'updater') is False:
-                        remove(session)
-                        print(sshx.Login(username, password, host, port, panel))
+                        sshx.Login(username, password, host, port, panel)
+                        if Path(session).is_file() is False:
+                            remove(session)
                         logs.writelines("[+] Login: " + host + "  " + panel + "  " + str(datetime.now()) + "\n")
                 except Exception as e:
-                    logs.writelines("[-] Error: " + str(e) + "    " + str(datetime.now()) + "\n")
+                    logs.writelines("[-] Session Error: " + str(e) + "    " + str(datetime.now()) + "\n")
             else:
-                logs.writelines("[-] Error: " + str(e) + "    " + str(datetime.now()) + "\n")
+                logs.writelines("[-] Session Error: " + str(e) + "    " + str(datetime.now()) + "\n")
 
 while True:
     main()
-    sleep(sleeper)
+    sleep(randint(300, 600))
