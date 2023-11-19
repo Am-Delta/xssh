962a4f8bd800256cea0eb9a2f2689ad0a68419e6
Am-Delta <140981321+Am-Delta@users.noreply.github.com>
2023-11-19 07:08:19 -0800

    optimized

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import re
+import gc
 import ast
 import sshx
 import json
@@ -1644,6 +1645,7 @@ def start_user(bot, message):
 
 @app.on_message(filters.private & filters.text)
 def text_private(bot, message):
+    gc.collect()
     chat_id = message.chat.id
     link = message.text
     if 'http://' in link:
