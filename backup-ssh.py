335ce189174ca808aa4608af7c8cb9304d71194a
Am-Delta <140981321+Am-Delta@users.noreply.github.com>
2023-11-18 17:51:27 -0800

    Update backup-ssh.py

--- backup-ssh.py
+++ backup-ssh.py
@@ -18,7 +18,10 @@ def main():
             Session = sshx.PANNEL(host, username, password, port, panel, 'Other', 'uname')
             status, content = Session.Backup_content()
             if status is True:
-                f = folder + "/" + host + ".sql"
+                if panel in ['dragon']:
+                    f = folder + "/" + host + ".vps"
+                else:
+                    f = folder + "/" + host + ".sql"
                 if Path(f).is_file() is True:
                     os.remove(f)
                 with open(f, 'wb') as file:
